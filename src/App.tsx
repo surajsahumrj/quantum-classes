5567850b040ccfd53f44ac53d41e69b4db79670a
nitishsinghbst <notadev.ui@gmail.com>
2026-09-17 00:51:36 +0530

    added google maps iframe in footer

--- src/App.tsx
+++ src/App.tsx
@@ -250,18 +250,17 @@ function Footer() {
             </a>
           </div>
         </div>
-      </div>
-      <div className="footer-map">
-        <iframe
-          title="Quantum Classes on Google Maps"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3564.879!2d83.4027785!3d26.787296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39914575a8c77589%3A0x492d0c31d836e486!2sQuantum%20Classes!5e0!3m2!1sen!2sin!4v1694000000000"
-          width="100%"
-          height="210"
-          style={{ border: 0 }}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
+        <div className="footer-map">
+          <iframe
+            title="Quantum Classes on Google Maps"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3564.879!2d83.4027785!3d26.787296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39914575a8c77589%3A0x492d0c31d836e486!2sQuantum%20Classes!5e0!3m2!1sen!2sin!4v1694000000000"
+            width="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
       </div>
       <div className="footer-bottom">
         <span>© 2026 Quantum Classes. All rights reserved.</span>
